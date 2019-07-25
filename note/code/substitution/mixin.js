a2e94973b0078dba02367d4ca77ae69afdd6bcca
op-y <ye.zhiqin@outlook.com>
2019-07-25 11:35:43 +0800

    tip 84 to 96

--- note/code/substitution/mixin.js
+++ note/code/substitution/mixin.js
@@ -10,4 +10,7 @@ mixin = {
     applyName: function(config) {
         return Factory(config, this.getName());
     },
+    applyNumber: function(config) {
+        return Factory(config, this.getNumber());
+    },
 }
